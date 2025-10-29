fe2f752619bfff4959575a24bc1dc6ccc2cdf7ae
Andrew Duffy <andrew@a10y.dev>
2025-10-29 11:02:30 -0400

    save
    Signed-off-by: Andrew Duffy <andrew@a10y.dev>

--- vortex-layout/src/layouts/list/reader/list.rs
+++ vortex-layout/src/layouts/list/reader/list.rs
@@ -9,20 +9,30 @@ use std::sync::Arc;
 
 use async_trait::async_trait;
 use vortex_array::MaskFuture;
-use vortex_dtype::{DType, FieldMask};
+use vortex_dtype::{DType, Field, FieldMask};
 use vortex_error::VortexResult;
 use vortex_expr::ExprRef;
 use vortex_mask::Mask;
 
+use crate::layouts::SharedArrayFuture;
 use crate::layouts::list::ListLayout;
 use crate::{ArrayFuture, LayoutReader, LayoutReaderRef};
 
+/// `LayoutReader` for the list layout holding
+/// `List`-typed data.
 pub struct ListReader {
     name: Arc<str>,
     layout: ListLayout,
     validity: Option<LayoutReaderRef>,
     offsets: LayoutReaderRef,
     elements: LayoutReaderRef,
+}
+
+impl ListReader {
+    fn offsets_array(&self) -> VortexResult<SharedArrayFuture> {
+        // Read via the layout to access the list offsets
+        self.layout
+    }
 }
 
 #[async_trait]
@@ -32,7 +42,7 @@ impl LayoutReader for ListReader {
     }
 
     fn dtype(&self) -> &DType {
-        todo!()
+        &self.layout.dtype
     }
 
     fn row_count(&self) -> u64 {
@@ -47,6 +57,16 @@ impl LayoutReader for ListReader {
     ) -> VortexResult<()> {
         // TODO(aduffy): What am I supposed to do for a FieldMask of vec![Field::ElementType] ?
 
+        if let Some(mask) = field_mask.first() {
+            match mask.starting_field()?.unwrap() {
+                Field::Name(_) => {
+                    // Invalid type
+                }
+                Field::ElementType => {}
+            }
+        }
+
+        // We split based on the number of reads of the offsets buffers here
         // Only register for the elements splits.
         self.elements
             .register_splits(field_mask, row_range, splits)?;
@@ -56,8 +76,8 @@ impl LayoutReader for ListReader {
 
     fn pruning_evaluation(
         &self,
-        row_range: &Range<u64>,
-        expr: &ExprRef,
+        _row_range: &Range<u64>,
+        _expr: &ExprRef,
         mask: Mask,
     ) -> VortexResult<MaskFuture> {
         // TODO(aduffy): if the root is an UNNEST, we can splat the mask out into the elements
@@ -67,9 +87,9 @@ impl LayoutReader for ListReader {
 
     fn filter_evaluation(
         &self,
-        row_range: &Range<u64>,
-        expr: &ExprRef,
-        mask: MaskFuture,
+        _row_range: &Range<u64>,
+        _expr: &ExprRef,
+        _mask: MaskFuture,
     ) -> VortexResult<MaskFuture> {
         todo!()
     }
@@ -80,7 +100,32 @@ impl LayoutReader for ListReader {
         expr: &ExprRef,
         mask: MaskFuture,
     ) -> VortexResult<ArrayFuture> {
-        // If we
+        // Expression should be root. We don't know how to handle other types of expressions
+        // on the root elements...do we?
         todo!()
     }
+}
+
+#[cfg(test)]
+mod tests {
+    //! List Layouts give us the opportunity to have deeply nested data accesses with the benefits
+    //! of full vectorization.
+    //!
+    //! Say that we have the following schema: `{a:list({b:i32?}?)}`
+    //!
+    //! This can be written with the following layout tree (some simple nodes elided):
+    //!
+    //! ```text
+    //! StructLayout
+    //! |__ a: ListLayout
+    //!     |__ offsets: FlatLayout
+    //!     |__ elements: StructLayout
+    //!         |__ b: ChunkedLayout
+    //!             |_ [0]: FlatLayout
+    //!             |_ [1]: FlatLayout
+    //!             |_ ...
+    //!             |_ [N]: FlatLayout
+    //! ```
+    //!
+    //! The ListLayout
 }
